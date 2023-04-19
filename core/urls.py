6172f0fdedf9c22e74c9c454415315ff3e1792ae
molatok <molatok@ya.ru>
2023-04-19 23:24:59 +0300

    registration

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegistrationView
 
 urlpatterns = [
-    path('core/sign-up/', UserRegistrationView.as_view(), name='user-registration'),
+    path('core/signup/', UserRegistrationView.as_view(), name='user-registration'),
 ]
--- lastwork/settings.py
+++ lastwork/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     "core",
     "drf_spectacular",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "lastwork.urls"
@@ -136,3 +139,6 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'core.CustomUser'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
