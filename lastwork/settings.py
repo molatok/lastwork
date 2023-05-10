91756ae07fd28589c2c7b6b8b4c3e3cb3e93f4bd
molatok <molatok@ya.ru>
2023-05-11 00:56:04 +0300

    ошибка yaml

--- lastwork/settings.py
+++ lastwork/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = "lastwork.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('POSTGRES_DB'),
-        'USER': env.str('POSTGRES_USER'),
-        'PASSWORD': env.str('POSTGRES_PASSWORD'),
-        'HOST': env.str('POSTGRES_HOST', default="localhost"),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str("POSTGRES_DB"),
+        'USER': env.str("POSTGRES_USER"),
+        'PASSWORD': env.str("POSTGRES_PASSWORD"),
+        'HOST': env.str("POSTGRES_HOST"),
+        'PORT': env.str("POSTGRES_PORT"),
     }
 }
 
