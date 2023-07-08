55251470cb809ab4329bf7541d56b8fa4ae553f0
molatok <molatok@ya.ru>
2023-07-08 20:46:18 +0400

    bot

--- lastwork/settings.py
+++ lastwork/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework.authtoken",
     'social_django',
+    'bot',
 ]
 
 MIDDLEWARE = [
--- lastwork/urls.py
+++ lastwork/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("core/", include("core.urls")),
     path("oauth/", include("social_django.urls", namespace="social")),
     path("goals/", include("goals.urls")),
+    path('bot/', include("bot.urls")),
 ]
